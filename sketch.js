3d96db1453b2f4b7e680fce0b17d5f0bbdc48c3e
ekambath <120076313+ekambath@users.noreply.github.com>
2022-12-07 20:06:57 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var square 
+
+function setup() {
+  createCanvas(400,400);
+  square = createSprite (200,200,40,40)
+  square.shapeColor = ("green")
+}
+
+
+
+function draw() 
+{
+  background(30);
+  drawSprites()
+}
+
+
+
+
